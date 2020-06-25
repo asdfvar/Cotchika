5967e0dfe5a8bb0c5b6e4689fce7182ce2d07f14
asdfvar <asdfvar@gmail.com>
2020-06-24 23:10:27 -0700

    added unique max-weight limits to Squiggly and Wiggly

--- kernel/derivative_units.cpp
+++ kernel/derivative_units.cpp
@@ -1,5 +1,11 @@
 #include "derivative_units.h"
 
-Squiggly::Squiggly (float position[3], MAP* Map) : Unit { position, Map } { }
+Squiggly::Squiggly (float position[3], MAP* Map) : Unit { position, Map }
+{
+   max_weight = 100000;
+}
 
-Wiggly::Wiggly (float position[3], MAP* Map) : Unit { position, Map } { }
+Wiggly::Wiggly (float position[3], MAP* Map) : Unit { position, Map }
+{
+   max_weight = 200000;
+}

5af9552cb546dbf191e7a84f206060010ea6efe4
asdfvar <asdfvar@gmail.com>
2021-05-07 22:18:48 -0700

    slight modification

--- cotchika_2/common.py
+++ cotchika_2/common.py
@@ -20,7 +20,7 @@ class Unit:
 # start - Tuple value indicating the starting cell
 # destination - Tuple value indicating the starting cell
 
-def generate_cost (domain, start, destination):
+def generate_cost (domain, resistance, start, destination):
    cost = np.zeros (np.shape (domain))
    cost[tuple (start)] = 1.0
    step_cost = 1.0
@@ -39,7 +39,7 @@ def generate_cost (domain, start, destination):
       min_dist = float ('inf')
       best_cell = perimeter[0]
       for cell in perimeter:
-         dest_dist = la.norm (np.array (destination) - np.array (cell)) + domain[cell]
+         dest_dist = la.norm (np.array (destination) - np.array (cell)) + resistance[cell]
          if dest_dist < min_dist:
             min_dist = dest_dist
             best_cell = cell
@@ -59,19 +59,18 @@ def generate_cost (domain, start, destination):
 
          if cost[new_cell] >= 0.0 and \
          all (np.array (new_cell) >= 0) and \
-         all (np.array (new_cell) < np.shape (domain)):
-            domain_cost = domain[new_cell]
-            if domain_cost >= 0.0:
-               step_dist  = la.norm (cell_offset)
+         all (np.array (new_cell) < np.shape (domain)) and \
+         domain[new_cell]:
+            step_dist  = la.norm (cell_offset)
 
-               # The cost is determined by a linear combination of the step distance and the
-               # domain cost added to the cost at the current cell
-               new_cost = cell_cost + step_dist + domain_cost
+            # The cost is determined by a linear combination of the step distance and the
+            # domain cost added to the cost at the current cell
+            new_cost = cell_cost + step_dist + resistance[new_cell]
 
-               if new_cost < cost[new_cell] or cost[new_cell] == 0.0:
-                  cost[new_cell] = new_cost
-                  if new_cell not in perimeter:
-                     perimeter.append (new_cell)
+            if new_cost < cost[new_cell] or cost[new_cell] == 0.0:
+               cost[new_cell] = new_cost
+               if new_cell not in perimeter:
+                  perimeter.append (new_cell)
 
          # Update the cell offset to the next N-D neighboring cell
          cell_offset[0] += 1
--- cotchika_2/test_1.py
+++ cotchika_2/test_1.py
@@ -4,24 +4,25 @@ import numpy as np
 import common
 from matplotlib import pyplot as plt
 
-domain      = np.zeros ((10, 10))
+domain      = np.ones ((10, 10), dtype = bool)
+resistance  = np.zeros ((10, 10))
 start       = (1, 1)
 destination = (8, 8)
 
 domain[2, 4] = -1.0
 
-domain[1, 3] = -1.0
-domain[2, 3] = -1.0
-domain[3, 3] = -1.0
-domain[3, 2] = -1.0
-domain[3, 1] = -1.0
+domain[1, 3] = False
+domain[2, 3] = False
+domain[3, 3] = False
+domain[3, 2] = False
+domain[3, 1] = False
 
-domain[7, 3] = 1.0
-domain[6, 3] = 1.0
-domain[6, 4] = 1.0
-domain[6, 5] = 1.0
+resistance[7, 3] = 1.0
+resistance[6, 3] = 1.0
+resistance[6, 4] = 1.0
+resistance[6, 5] = 1.0
 
-cost = common.generate_cost (domain, start, destination)
+cost = common.generate_cost (domain, resistance, start, destination)
 
 plt.imshow (cost)
 plt.colorbar ()
